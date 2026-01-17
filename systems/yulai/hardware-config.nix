1e2c09e0565091a6800220099bb1ed72b6e1ce73
Aurelia <git@acuteaura.net>
2026-01-17 19:15:22 +0100

    update from cyberdaemon@2026-01-17 18:15:22+0000

--- systems/yulai/hardware-config.nix
+++ systems/yulai/hardware-config.nix
@@ -6,11 +6,6 @@
   boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
   boot.initrd.kernelModules = ["nvme" "virtio_gpu"];
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/227C-A484";
-    fsType = "vfat";
-  };
-
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/64f6fb02-4fed-4836-a33b-86e8993afdfa";
     fsType = "ext4";
