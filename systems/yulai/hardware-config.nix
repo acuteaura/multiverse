a48fd8a2e907d830fe5e2655d41f3e8090b5bd70
Aurelia <git@acuteaura.net>
2026-01-17 19:07:27 +0100

    update from cyberdaemon@2026-01-17 18:07:27+0000

--- systems/yulai/hardware-config.nix
+++ systems/yulai/hardware-config.nix
@@ -3,16 +3,16 @@
   boot.loader.systemd-boot.enable = true;
 
   boot.kernelParams = ["console=tty"];
-  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront"];
+  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
   boot.initrd.kernelModules = ["nvme" "virtio_gpu"];
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/7763-8830";
+    device = "/dev/disk/by-uuid/227C-A484";
     fsType = "vfat";
   };
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/7e1b5c37-1536-47c9-b5a0-456340d2327f";
+    device = "/dev/disk/by-uuid/64f6fb02-4fed-4836-a33b-86e8993afdfa";
     fsType = "ext4";
   };
 }
