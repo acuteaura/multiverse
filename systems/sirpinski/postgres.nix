0f338aa974e731df715872684717ba98b9f847e8
Aurelia <git@acuteaura.net>
2026-02-13 21:14:18 +0100

    update from MacBookPro.internal@2026-02-13 20:14:18+0000

--- systems/sirpinski/postgres.nix
+++ systems/sirpinski/postgres.nix
@@ -1,19 +1,25 @@
 {lib, ...}: {
   services.postgresql = {
     enable = true;
-    ensureDatabases = ["akkoma"];
+    ensureDatabases = ["akkoma" "gotosocial"];
     ensureUsers = [
       {
         name = "akkoma";
         ensureDBOwnership = true;
         ensureClauses.login = true;
       }
+      {
+        name = "gotosocial";
+        ensureDBOwnership = true;
+        ensureClauses.login = true;
+      }
     ];
     identMap = ''
       # ArbitraryMapName systemUser DBUser
-      superuser_map      root      postgres
-      superuser_map      postgres  postgres
-      superuser_map      akkoma    akkoma
+      superuser_map      root        postgres
+      superuser_map      postgres    postgres
+      superuser_map      akkoma      akkoma
+      superuser_map      gotosocial  gotosocial
     '';
     authentication = lib.mkOverride 10 ''
       #type database  DBuser    auth-method optional_ident_map
