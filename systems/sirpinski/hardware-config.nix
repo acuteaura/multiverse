8fbd154ecd21c7597c5802480943e7a1d7de15ca
Aurelia <git@acuteaura.net>
2026-07-03 12:57:23 +0200

    update from meowbaseone@2026-07-03 10:57:23+0000

--- systems/sirpinski/hardware-config.nix
+++ systems/sirpinski/hardware-config.nix
@@ -5,6 +5,8 @@
   boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
   boot.initrd.kernelModules = ["nvme" "virtio_gpu"];
 
+  boot.zfs.forceImportRoot = false;
+
   disko = {
     devices = {
       disk = {
