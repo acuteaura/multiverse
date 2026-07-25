3b49e4fb8e65f5716675f4b6af655e26dbddca78
Aurelia <git@acuteaura.net>
2026-07-25 17:42:47 +0200

    update from meowbaseone@2026-07-25 15:42:47+0000

--- systems/sirpinski/keycloak.nix
+++ systems/sirpinski/keycloak.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{pkgs, config, ...}: {
   services.keycloak = {
     enable = true;
     database = {
@@ -19,15 +19,18 @@
     };
   };
 
-  services.nginx = {
+  services.nginx = let
+    kcPort = config.services.keycloak.settings.http-port;
+  in {
     virtualHosts."id.nullvoid.space" = {
       forceSSL = true;
       kTLS = true;
       enableACME = true;
       locations."/" = {
-        proxyPass = "http://127.0.0.1:8080";
+        proxyPass = "http://127.0.0.1:${toString kcPort}";
         proxyWebsockets = true;
         extraConfig = ''
+          proxy_set_header X-Forwarded-For $remote_addr;
           client_max_body_size 256M;
           allow 100.64.0.0/10;
           allow 89.1.7.228;
@@ -35,24 +38,34 @@
         '';
       };
       locations."/realms/nvs" = {
-        proxyPass = "http://127.0.0.1:8080";
+        proxyPass = "http://127.0.0.1:${toString kcPort}";
         proxyWebsockets = true;
         extraConfig = ''
+          proxy_set_header X-Forwarded-For $remote_addr;
           client_max_body_size 256M;
         '';
       };
       locations."/realms/nullvoid.space" = {
-        proxyPass = "http://127.0.0.1:8080";
+        proxyPass = "http://127.0.0.1:${toString kcPort}";
         proxyWebsockets = true;
         extraConfig = ''
+          proxy_set_header X-Forwarded-For $remote_addr;
           client_max_body_size 256M;
         '';
       };
       locations."/resources" = {
-        proxyPass = "http://127.0.0.1:8080";
+        proxyPass = "http://127.0.0.1:${toString kcPort}";
         proxyWebsockets = true;
+        extraConfig = ''
+          proxy_set_header X-Forwarded-For $remote_addr;
+        '';
       };
       locations."/realms/nvs/metrics" = {
+        extraConfig = ''
+          deny all;
+        '';
+      };
+      locations."/realms/nullvoid.space/metrics" = {
         extraConfig = ''
           deny all;
         '';
