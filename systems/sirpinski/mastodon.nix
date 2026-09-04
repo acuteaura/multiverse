4d8dd30211d94afb39be77c3860d5cb77ec3680d
Aurelia <git@acuteaura.net>
2026-09-04 16:12:15 +0200

    update from meowbaseone@2026-09-04 14:12:15+0000

--- systems/sirpinski/mastodon.nix
+++ systems/sirpinski/mastodon.nix
@@ -7,11 +7,13 @@
   webDomain = "social.problematic.solutions";
   streamingProcesses = 1;
 in {
+
   services.mastodon = {
     enable = true;
     localDomain = federationDomain;
     extraConfig.WEB_DOMAIN = webDomain;
-    configureNginx = false;
+    redis.createLocally = true;
+    configureNginx = true;
     inherit streamingProcesses;
     database = {
       createLocally = true;
@@ -26,45 +28,6 @@ in {
   };
 
   services.nginx = {
-    upstreams.mastodon-streaming = {
-      extraConfig = "least_conn;";
-      servers = builtins.listToAttrs (
-        map (i: {
-          name = "unix:/run/mastodon-streaming/streaming-${toString i}.socket";
-          value = {};
-        }) (lib.range 1 streamingProcesses)
-      );
-    };
-
-    virtualHosts.${webDomain} = {
-      root = "${config.services.mastodon.package}/public/";
-      forceSSL = true;
-      enableACME = true;
-
-      extraConfig = ''
-        client_max_body_size 100m;
-      '';
-
-      locations."/system/".alias = "/var/lib/mastodon/public-system/";
-
-      locations."/" = {
-        tryFiles = "$uri @proxy";
-      };
-
-      locations."@proxy" = {
-        proxyPass = "http://unix:/run/mastodon-web/web.socket";
-        proxyWebsockets = true;
-        recommendedProxySettings = true;
-      };
-
-      locations."/api/v1/streaming" = {
-        proxyPass = "http://mastodon-streaming";
-        proxyWebsockets = true;
-        recommendedProxySettings = true;
-      };
-    };
-
-    # redirects, relocate block if anything else ends up here
     virtualHosts.${federationDomain} = {
       forceSSL = true;
       kTLS = true;
@@ -75,4 +38,7 @@ in {
       locations."/".return = "301 https://${webDomain}";
     };
   };
+
+  services.opensearch.enable = true;
+  services.mastodon.elasticsearch.host = "127.0.0.1";
 }
